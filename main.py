8301fd5cb0e8613c810a9eeb6536ae0b9d94940b
Ruben <rbenbal43@gmail.com>
2025-06-06 14:37:54 -0500

    convertir texto plano a word

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-from PyPDF import PdfReader
+import sys
+from datetime import datetime
+from PyPDF2 import PdfReader
 from docx import Document
+
+documento_pdf = sys.argv[1]
+documento_word = Document()
+
+reader = PdfReader(documento_pdf)
+text = ""
+
+for page in reader.pages:
+    text += page.extract_text()
+
+documento_word.add_paragraph(text)
+name = str(datetime.now())
+documento_word.save(f'/home/ruben/Documentos/{name}.docx')
